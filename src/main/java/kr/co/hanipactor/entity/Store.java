8a439f399a14417f1237742482b33044a3fd9e7d
habi802 <shb8002@gmail.com>
2025-09-01 14:50:56 +0900

    [refactor] yml DB 변경

---
+++ src/main/java/kr/co/hanipactor/entity/Store.java
@@ -0,0 +1,13 @@
+package kr.co.hanipactor.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}
