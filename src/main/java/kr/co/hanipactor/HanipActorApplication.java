3411788e92a12dd37c52adb29bfc3e16c2b7aa51
habi802 <shb8002@gmail.com>
2025-09-01 11:41:39 +0900

    [init] first

---
+++ src/main/java/kr/co/hanipactor/HanipActorApplication.java
@@ -0,0 +1,13 @@
+package kr.co.hanipactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanipActorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanipActorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/hanipactor/HanipActorApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.hanipactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanipActorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
