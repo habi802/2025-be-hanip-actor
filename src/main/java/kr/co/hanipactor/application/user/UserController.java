1f90d9324f0721a3382bfb8a289557bba6424294
nususu1004 <nususu1004@gmail.com>
2025-09-01 18:10:28 +0900

    jwt 처리중 2

--- src/main/java/kr/co/hanipactor/application/user/UserController.java
+++ src/main/java/kr/co/hanipactor/application/user/UserController.java
@@ -1,14 +1,18 @@
 package kr.co.hanipactor.application.user;
 
+import kr.co.hanipactor.config.jwt.JwtTokenManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final JwtTokenManager jwtTokenManager;
+
 }
--- src/main/java/kr/co/hanipactor/application/user/UserService.java
+++ src/main/java/kr/co/hanipactor/application/user/UserService.java
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserMapper userMapper;
+
 }
