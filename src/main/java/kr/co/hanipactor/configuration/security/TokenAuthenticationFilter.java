8af6f4dc059f2a3ad6d5d5365aebf1e993891161
habi802 <shb8002@gmail.com>
2025-09-15 11:07:36 +0900

    [feat] 내부 서비스끼리의 api 호출 관련 코드 추가

--- src/main/java/kr/co/hanipactor/configuration/security/TokenAuthenticationFilter.java
+++ src/main/java/kr/co/hanipactor/configuration/security/TokenAuthenticationFilter.java
@@ -4,12 +4,14 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.ws.rs.core.HttpHeaders;
 import kr.co.hanipactor.configuration.jwt.JwtTokenManager;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
