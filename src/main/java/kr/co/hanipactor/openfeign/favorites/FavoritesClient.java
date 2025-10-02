e3a7044f5ab2f40c77c529303f44692ff225c126
habi802 <shb8002@gmail.com>
2025-10-02 17:17:39 +0900

    [fix] 배포 환경 관련 오류 수정

--- src/main/java/kr/co/hanipactor/openfeign/favorites/FavoritesClient.java
+++ src/main/java/kr/co/hanipactor/openfeign/favorites/FavoritesClient.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(
-        name = "${constants.open-feign.action.name:hanip-action}",
+        name = "hanip-action",
         contextId = "favoriteClient",
-        url = "${constants.open-feign.action.url:}",
+        url = "${constants.open-feign.action.url}",
         configuration = FeignConfiguration.class)
 public interface FavoritesClient {
     @GetMapping("/api/favorite/count")
