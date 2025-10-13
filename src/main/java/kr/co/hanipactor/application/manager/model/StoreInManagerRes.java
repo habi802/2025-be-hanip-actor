2f0db8d17c2008d600a4079e187c366fcb27a3c9
habi802 <shb8002@gmail.com>
2025-10-13 11:04:45 +0900

    [feat] 가게 상세 조회 사업자 등록증 조회를 위한 유저 id 필드 추가, 사업자 등록 번호 표시 형식 추가

--- src/main/java/kr/co/hanipactor/application/manager/model/StoreInManagerRes.java
+++ src/main/java/kr/co/hanipactor/application/manager/model/StoreInManagerRes.java
@@ -11,6 +11,7 @@ import java.util.List;
 @ToString
 public class StoreInManagerRes {
     private long storeId;
+    private long userId;
     private String name;
     private String imagePath;
     private List<String> categories;
